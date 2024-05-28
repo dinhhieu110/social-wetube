46739fa63cd0aec5fd97413eb5549c3862d30383
dinhhieu110 <trandinhhieu11002@gmail.com>
2024-05-29 03:00:26 +0700

    Home Video Card

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { Container } from '@mui/material';
+import CardVideoHome from '../../components/card-video-home';
+
+import { videos } from './mock';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="grid gap-6 grid-cols-3 m-12">
+      {videos.map((video) => (
+        <CardVideoHome video={video} key={video._id} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
